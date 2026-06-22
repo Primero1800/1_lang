d9f33fafab5896774c3b70085aee7b08655ee1ce
Primero1800 <Primero@inbox.ru>
2026-06-22 15:13:09 +0300

    feat: tokens model added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
+from app.models.ai_token_usage import AiTokenUsage
 from app.models.base import Base
 from app.models.phrase_data import PhraseData
 from app.models.phrase_embeddings import PhraseEmbedding
 from app.models.phrases import Phrase
 
-__all__ = ("Base", "Phrase", "PhraseData", "PhraseEmbedding")
+__all__ = ("AiTokenUsage", "Base", "Phrase", "PhraseData", "PhraseEmbedding")
